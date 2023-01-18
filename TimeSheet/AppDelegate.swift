ec9cd1b90dbce95b03a99a0e2293a86485168b33
Keiichi Okamoto <okamoto@jcom.home.ne.jp>
2023-01-18 17:32:35 +0900

    add yyy()

--- TimeSheet/AppDelegate.swift
+++ TimeSheet/AppDelegate.swift
@@ -12,9 +12,12 @@ import CoreData
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func xxx() {
-        
+        print("xxx")
     }
 
+    func yyy() {
+        
+    }
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
